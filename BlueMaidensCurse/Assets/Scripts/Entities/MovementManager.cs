ed7dd4d060a41fd10064abcfea17435508b14333
Gabu <Gabriel.Senekovic@gmail.com>
2020-02-06 19:39:13 +0100

    Enemy Movement + other stuff
    There is now a white box base enemy that moves after the players position. Trees are now editable from the inspector. I also rearranged the object structure.

---
+++ BlueMaidensCurse/Assets/Scripts/Entities/MovementManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour //This is the script that should be used for everything that moves!
+{
+    Rigidbody2D myBody;
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+    public void Move(Vector2 movementDirection)
+    {
+        Debug.Log(movementDirection);
+        myBody.velocity = movementDirection;
+    }
+}
---
+++ BlueMaidensCurse/Assets/Scripts/Game Management/SpriteLibrary.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode] public class SpriteLibrary : MonoBehaviour
+{
+    public static SpriteLibrary I; //Instance of this sprite library, so that it can be accessed from anywhere
+
+
+    public void Update()
+    {
+        if(I == null)
+        {
+            I = this; //Sets the instance to be this object every time the editor is updated
+        }
+    }
+
+    public Sprite[] treeSprites;
+
+    public Sprite GetSprite(TreeType type) //This gives back the correct sprite to the tree
+    {
+        switch (type) //The switch statement is better when checking an enum than an if statement would be
+        {
+            case TreeType.Tree1:
+                return treeSprites[0];
+            case TreeType.Tree2:
+                return treeSprites[1];
+            case TreeType.Tree3:
+                return treeSprites[2];
+            case TreeType.Tree4:
+                return treeSprites[3];
+            case TreeType.Tree5:
+                return treeSprites[4];
+            case TreeType.Tree6:
+                return treeSprites[5];
+            case TreeType.Tree7:
+                return treeSprites[6];
+            default:
+                return treeSprites[0];
+        }
+    }
+}
