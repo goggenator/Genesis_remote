4113c23bd04a1acff835e6e9cc128df156f56812
Nicolas Hammar <nico.hammar@gmail.com>
2020-02-11 17:21:52 +0100

    Added a pause menu and UI folders for scripts and prefabs, as well as a UI canvas

---
+++ BlueMaidensCurse/Assets/Scripts/UI scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log ("I hope you are proud of yourself");
+        Application.Quit();
+    }
+}
