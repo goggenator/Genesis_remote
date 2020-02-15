22fcec76b820a142393426f6a1b51107df46f4e7
Gabu <Gabriel.Senekovic@gmail.com>
2020-02-15 16:47:30 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ BlueMaidensCurse/Assets/Scripts/Entities/Curse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curse : MonoBehaviour
+{
+    float curseDamage = 0.01f;
+    HealthManager HP;
+
+    public void Awake()
+    {
+        HP = GetComponent<HealthManager>();
+    }
+
+    public void Update()
+    {
+        HP.OnHit(curseDamage * Time.deltaTime);
+    }
+}
---
+++ BlueMaidensCurse/Assets/Scripts/UI scripts/HPBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+    public HealthManager HP;
+    [SerializeField] Image HealthBar;
+    public Text HealthText;
+    private float currentFill;
+    public float BarChangeSpeed;
+
+    private void Update()
+    {
+        UpdateText();
+        UpdateHealthBar();
+    }
+    void UpdateText()
+    {
+        if (HealthText != null)
+        {
+            HealthText.text = Mathf.RoundToInt(HP.GetHP()) + "/" + Mathf.RoundToInt(HP.GetMaxHP());
+        }
+
+    }
+    private void UpdateHealthBar()
+    {
+        currentFill = HP.GetHPPercentage();
+        if (currentFill != HealthBar.fillAmount)
+        {
+            HealthBar.fillAmount = Mathf.Lerp(HealthBar.fillAmount, currentFill, Time.deltaTime * BarChangeSpeed);
+        }
+    }
+}
