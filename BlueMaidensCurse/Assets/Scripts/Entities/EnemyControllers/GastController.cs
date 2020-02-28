51459fe8ef9f267ac34a51d51edc5e938a8e5565
Gabu <Gabriel.Senekovic@gmail.com>
2020-02-28 22:19:31 +0100

    Gast now does not come close to player

---
+++ BlueMaidensCurse/Assets/Scripts/Entities/EnemyControllers/GastController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GastController : EnemyController
+{
+    [SerializeField] bool walking = false;
+    [SerializeField]float waitUntilChangeDirection;
+    [SerializeField] float distanceFromPlayerAllowed;
+    private void Awake()
+    {
+        movement = GetComponent<MovementManager>();
+        HP = GetComponentInChildren<HealthManager>();
+        HP.InitializeHealth(HP.GetMaxHP());
+        targetPosition = Vector2.zero;
+    }
+    public virtual void Update()
+    {
+        if(!walking)
+        {
+            StartCoroutine(WaitForDirection());
+        }
+        if (GetComponent<SpiralShooting>())
+        {
+            if(!GetComponent<SpiralShooting>().GetIsShooting())
+            {
+                GetComponent<SpiralShooting>().SetIsShooting(true);
+            }
+        }
+        movement.SetDirection(targetPosition);
+    }
+
+    void ChangeDirection()
+    {
+        Vector2 distance = new Vector2(Game.I.GetPlayerPosition().x - transform.position.x, Game.I.GetPlayerPosition().y - transform.position.y);
+        if (distance.magnitude < distanceFromPlayerAllowed)
+        {
+            distance = new Vector2(transform.position.x - Game.I.GetPlayerPosition().x, transform.position.y - Game.I.GetPlayerPosition().y);
+        }
+
+        if ((distance.x > distance.y && distance.x > 0 && distance.y > 0) || (distance.x < distance.y && distance.x < 0 && distance.y < 0))
+        {
+            distance.y = 0;
+        }
+        else
+        {
+            distance.x = 0;
+        }
+        distance.Normalize();
+        targetPosition = distance;
+    }
+
+    IEnumerator WaitForDirection()
+    {
+        walking = true;
+        yield return new WaitForSeconds(waitUntilChangeDirection);
+        ChangeDirection();
+        walking = false;
+    }
+}
